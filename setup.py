b4d4c8c3a1c1760639abf41513d7022fb6f04285
Faisal ALSHARGI <alshargi@hotmail.de>
2023-08-18 00:02:57 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,4 @@
 # -*- coding: utf-8 -*-
-
 import setuptools
 
 with open("README.md", "r", encoding="utf-8") as fh:
@@ -7,18 +6,19 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name='HarassmentClassifier',
-    version='0.0.2',
+    version='0.0.3',
     author='Faisal Alshargi',
     author_email='alshargi@hotmail.de',
     description='Arabic Harassment classifier',
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url='https://github.com/alshargi/HarassmentClassifier.git',
-    project_urls = {
-        "Bug Tracker": "https://github.com/alshargi/HarassmentClassifier.git/issues"
+    url='https://github.com/alshargi/HarassmentClassifier',
+    project_urls={
+        "Bug Tracker": "https://github.com/alshargi/HarassmentClassifier/issues"
     },
     license='MIT',
     packages=['HarassmentClassifier'],
-    package_data={'HarassmentClassifier': ['haras/*.sav']}, 
+    package_data={'HarassmentClassifier': ['haras/*.sav']},
     install_requires=['requests'],
 )
+
